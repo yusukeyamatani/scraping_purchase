b8d8598e7713c12aaa7b777031c135491857a549
優介 山谷 <yusuke.yamatani@gu3.co.jp>
2016-09-21 03:48:15 +0900

    amazon multi-thread

--- scripts/amazon_scraping.py
+++ scripts/amazon_scraping.py
@@ -1,114 +1,149 @@
 # -*- coding: utf-8 -*-
 """
-http://selenium-python.readthedocs.io/locating-elements.html#locating-by-xpath
+Amazon
 """
 import os
-import time
 import sys
+import threading
 
 path = os.path.join(os.path.dirname(__file__), '../')
 sys.path.append(path)
 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+from base import BasePurchase, Finish
+
+path = os.path.join(os.path.dirname(__file__), '../')
+sys.path.append(path)
+
 from settings.amazon import (LOGIN_URL,
                              LOGOUT_URL,
                              PRODUCT_URL,
                              ID,
                              PASSWORD,
                              )
+CART_TYPE = ['one_click', 'normal']
 
-
-DRIVER_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'driver', 'chromedriver'))
 RETRY_COUNT = 100
+THREAD_NUM = 5
 IS_DEBUG = True
-DRIVER = webdriver.Chrome(DRIVER_PATH)
 
 
-class CartException(Exception):
+class Finish(object): pass
+ns = Finish()
+ns.end_flag = False
+
+
+class CartExist(Exception):
     pass
 
 
-def product_purchase():
-    _login()
-    DRIVER.get(PRODUCT_URL)
-    _add_cart()
-    _procedures()
-    _purchase()
+class AmazonPurchase(BasePurchase):
 
+    def product_purchase(self):
+        self._login()
+        self.driver.get(PRODUCT_URL)
+        self._add_cart()
+        self._procedures()
+        self._purchase()
 
-def _element_exists(m, args):
-    try:
-        return m(args)
-    except NoSuchElementException:
-        return None
+    def _login(self):
+        print 'ログイン'
 
+        self.driver.get(LOGIN_URL)
 
-def _login():
-    print(DRIVER.current_url)
-    print('ログイン')
-    DRIVER.get(LOGIN_URL)
-    time.sleep(1) # 画面表を待つ 秒数は適当
-    DRIVER.find_element_by_id('ap_email').send_keys(ID)
-    DRIVER.find_element_by_id('ap_password').send_keys(PASSWORD)
-    DRIVER.find_element_by_id('signInSubmit').click()
+        self.wait.until(EC.presence_of_element_located((By.ID, 'ap_email')))
 
+        self.driver.find_element_by_id('ap_email').send_keys(ID)
+        self.driver.find_element_by_id('ap_password').send_keys(PASSWORD)
+        self.driver.find_element_by_id('signInSubmit').click()
 
-def _add_cart():
-    print(DRIVER.current_url)
+    def _add_cart(self):
+        def _one_click_open_link_exists():
+            try:
+                return EC.presence_of_element_located((By.LINK_TEXT, '1-Clickで注文する場合は、サインインをしてください。'))
+            except NoSuchElementException:
+                return None
 
-    def _click_cart():
+        def _one_click_exists():
+            try:
+                return EC.presence_of_element_located((By.ID, 'oneClickBuyButton'))
+            except NoSuchElementException:
+                return None
 
-        link = _element_exists(DRIVER.find_element_by_link_text, '1-Clickで注文する場合は、サインインをしてください。')
-        if link:
-            link.click()
+        def _cart_exists():
+            try:
+                return self.wait.until(EC.presence_of_element_located((By.ID, 'submit.add-to-cart')))
+            except NoSuchElementException:
+                return None
 
-        if not IS_DEBUG:
-            one_click_cart = _element_exists(DRIVER.find_element_by_id, 'oneClickBuyButton')
-            if one_click_cart:
-                one_click_cart.click()
-        else:
-            one_click_cart = None
+        def _click_cart_type():
+            link = _one_click_open_link_exists()
+            if link:
+                self.driver.find_element_by_link_text('1-Clickで注文する場合は、サインインをしてください。').click()
 
-        if not one_click_cart:
-            add_cart = DRIVER.find_element_by_id('submit.add-to-cart')
-            add_cart.click()
+            if _one_click_exists():
+                return CART_TYPE[0]
 
-    DRIVER.get(PRODUCT_URL)
-    for i in range(1, RETRY_COUNT):
-        try:
-            _click_cart()
-        except NoSuchElementException:
-            print('reload')
-            DRIVER.execute_script('location.reload()')
+            if _cart_exists:
+                return CART_TYPE[1]
 
+            return None
 
-def _procedures():
-    print(DRIVER.current_url)
-    print('購入手続き')
-    cart_btn = DRIVER.find_element_by_id('hlb-ptc-btn')
-    cart_btn.click()
+        self.driver.get(PRODUCT_URL)
+        for i in range(1, RETRY_COUNT):
+            cart_type = _click_cart_type()
+            if cart_type:
+                break
+            else:
+                print 'reload'
+                self.driver.execute_script('location.reload()')
+        print cart_type
+        if cart_type == CART_TYPE[0]:
+            if IS_DEBUG:
+                print 'DEBUG one click'
+                cart_type = CART_TYPE[1]
+            else:
+                self.driver.find_element_by_id('oneClickBuyButton').click()
+        print cart_type
+        if cart_type == CART_TYPE[1]:
+            self.driver.find_element_by_id('submit.add-to-cart').click()
 
+        if not cart_type:
+            raise CartExist
 
-def _purchase():
-    print(DRIVER.current_url)
-    url = DRIVER.current_url
-    if url == 'https://www.amazon.co.jp/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1':
-        print('最終決済')
+    def _procedures(self):
+        print '購入手続き'
+
+        self.wait.until(EC.presence_of_element_located((By.ID, 'hlb-ptc-btn')))
+
+        cart_btn = self.driver.find_element_by_id('hlb-ptc-btn')
+        cart_btn.click()
+
+    def _purchase(self):
+        if ns.end_flag:
+            print 'not _purchase'
+            return
+
+        print '最終決済'
+        self.wait.until(EC.presence_of_element_located((By.NAME, 'placeYourOrder1')))
+
         if not IS_DEBUG:
             # ↓最終決済
-            DRIVER.find_element_by_name('placeYourOrder1').click()
+            self.driver.find_element_by_name('placeYourOrder1').click()
         else:
-            raise
+            print 'DEBUG _purchase'
+        ns.end_flag = True
+        return
 
 if __name__ == '__main__':
-    DRIVER.set_window_size(1200, 1000)
-    url = (DRIVER.current_url)
-    print url
-    try:
-        product_purchase()
-    except:
-        pass
-    finally:
-        print('ログアウト')
-        DRIVER.get(LOGOUT_URL)
+    threads = []
+    for i in range(THREAD_NUM):
+        test = AmazonPurchase()
+        t = threading.Thread(target=test.product_purchase)
+        threads.append(t)
+        t.start()
